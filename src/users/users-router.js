17c22252ae42b6dfb4b4902534b4b0eca80117ec
Edwin Qiu <edwinqiu93@gmail.com>
2019-06-19 02:31:54 -0700

    finished register route endpoint

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,6 +1,6 @@
 //need get for intial app componentdidMount, gets all users in the db and sets it as the state
 //post
-
+const path = require('path')
 const express = require('express')
 const UsersRouter = express.Router()
 const jsonParser = express.json()
@@ -54,7 +54,49 @@ UsersRouter
 UsersRouter
     .route('/register')
     .post(jsonParser, (req, res, next) => {
+        const { password, username } = req.body
+        const db = req.app.get('db')
 
+
+    for (const field of ['username', 'password'])
+      if (!req.body[field])
+        return res.status(400).json({
+          error: `Missing '${field}' in request body`
+        })
+
+    const passwordError = UsersService.validatePassword(password)
+
+    if (passwordError)
+      return res.status(400).json({ error: passwordError })
+
+    UsersService.hasUserWithUserName(
+      db,
+      username
+    )
+      .then(hasUserWithUserName => {
+        if (hasUserWithUserName)
+          return res.status(400).json({ error: `Username already taken` })
+
+        return UsersService.hashPassword(password)
+          .then(hashedPassword => {
+            const newUser = {
+              username,
+              password: hashedPassword
+            }
+
+            return UsersService.insertUser(
+              db,
+              newUser
+            )
+              .then(user => {
+                res
+                  .status(201)
+                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                  .json(UsersService.serializeUser(user))
+              })
+          })
+      })
+      .catch(next)
     })
 
 
