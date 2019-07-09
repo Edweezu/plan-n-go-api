a1ea6feac5d62578570d32e559a79ceea99deed7
Edwin Qiu <edwinqiu93@gmail.com>
2019-07-08 23:09:14 -0700

    fixed create account error

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,5 +1,3 @@
-//need get for intial app componentdidMount, gets all users in the db and sets it as the state
-//post
 const path = require('path')
 const express = require('express')
 const UsersRouter = express.Router()
